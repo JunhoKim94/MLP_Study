a3f1860e4e274e2504b91034cdc59f52d5626f5c
junho <junho9000@naver.com>
2020-11-23 00:47:49 +0900

    latest

--- optim/optimizer.py
+++ optim/optimizer.py
@@ -1,28 +1,17 @@
 import numpy as np
 
-
 class SGD:
     def __init__(self):
-        # ========================= EDIT HERE =========================
-        pass
-
-
-        # =============================================================
 
     def update(self, w, grad, lr):
         updated_weight = None
-        # ========================= EDIT HERE =========================
         updated_weight = w - lr * grad
 
-
-        # =============================================================
         return updated_weight
 
 class Momentum:
     def __init__(self, gamma):
-        # ========================= EDIT HERE =========================
         self.gamma = gamma
-        # =============================================================
         self.v = dict()
 
     def update(self, w, grad, lr):
@@ -30,9 +19,6 @@ class Momentum:
         if self.v.get(grad.shape) is None:
             self.v[grad.shape] = np.zeros_like(grad)
         
-        #print(len(self.v))
-        # ========================= EDIT HERE =========================
-        #print(len(self.v), grad.shape, self.v)
         '''
         self.v[grad.shape] = self.gamma * self.v[grad.shape] + lr * grad
         updated_weight = w - self.v[grad.shape]
@@ -40,35 +26,26 @@ class Momentum:
         '''
         self.v[grad.shape] = self.gamma * self.v[grad.shape] + grad
         updated_weight = w - lr * self.v[grad.shape]
-        
 
-        # =============================================================
         return updated_weight
 
 
 class RMSProp:
-    # ========================= EDIT HERE =========================
     def __init__(self, gamma, epsilon):
-        # ========================= EDIT HERE =========================
         self.gamma = gamma
         self.epsilon = epsilon
         self.G = dict()
 
-        # =============================================================
 
     def update(self, w, grad, lr):
         updated_weight = None
-        # ========================= EDIT HERE =========================
         if self.G.get(grad.shape) is None:
             self.G[grad.shape] = np.zeros_like(grad)
 
         self.G[grad.shape] = self.gamma*self.G[grad.shape] + (1-self.gamma)*(grad**2)
-        #print(self.G.shape,grad.shape)
         updated_weight = w - (lr/np.sqrt(self.epsilon+self.G[grad.shape]))*grad
 
 
-
-        # =============================================================
         return updated_weight
 
 class Adam:
